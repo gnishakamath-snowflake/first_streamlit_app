8d9b104999608d8be2cb37b5d0b3b9da05690586
Nisha G Kamath <117295761+gnishakamath-snowflake@users.noreply.github.com>
2022-11-02 21:47:09 +0530

    Update streamlit_app.py
    Added More Items  for bf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 
 import streamlit
-streamlit.title("My Parents New Healthy Diner")
+streamlit.title("My Mom's New Healthy Diner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Dosa")
-streamlit.text("Idly")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
