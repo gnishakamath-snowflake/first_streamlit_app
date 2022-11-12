cb29244dd10b6b7c04c793f58c2120950acef72c
Nisha G Kamath <117295761+gnishakamath-snowflake@users.noreply.github.com>
2022-11-12 19:03:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,22 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title("My Mom's New Healthy Diner")
+def get_fv_data(fv_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fv_choice)
+  #streamlit.text(fruityvice_response.json()) #Just displays data in JSON 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # Takes JSON output and normaizes the same 
+  return(fruityvice_normalized)
 
+def get_sf_FRUIT_LOAD_LIST():
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_cur = my_cnx.cursor()
+  #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+  #my_data_row = my_cur.fetchone()
+  my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+  return(my_cur.fetchall())  
+
+
+streamlit.title("My Mom's New Healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -30,11 +44,6 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-def get_fv_data(fv_choice):
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fv_choice)
-  #streamlit.text(fruityvice_response.json()) #Just displays data in JSON 
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # Takes JSON output and normaizes the same 
-  return(fruityvice_normalized)
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -48,17 +57,14 @@ try:
 except URLError as e:
   streamlit.error()
   
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
-#my_data_row = my_cur.fetchone()
-my_data_row = my_cur.fetchall()
+
+
 streamlit.header("The fruit load list contains :")
-streamlit.dataframe(my_data_row)
+if streamlit.button('Get Fruit Load List'):
+  fv_list = get_sf_FRUIT_LOAD_LIST()
+  streamlit.dataframe(fv_list)
 
-
+streamlit.stop()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write('Thaks for adding ', add_my_fruit)
 my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('From STREAM LIT')")
