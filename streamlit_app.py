bc37446c634c7e2eb305a142eeec555351956405
Nisha G Kamath <117295761+gnishakamath-snowflake@users.noreply.github.com>
2022-11-12 18:46:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -30,15 +31,16 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json()) #Just displays data in JSON format
-
-# Takes JSON output and normaizes the same 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# displays the data in tabular format
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get the information")
+  else:
+    streamlit.write('The user entered ', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    #streamlit.text(fruityvice_response.json()) #Just displays data in JSON 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # Takes JSON output and normaizes the same 
+    streamlit.dataframe(fruityvice_normalized) # displays the data in tabular format
 
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
